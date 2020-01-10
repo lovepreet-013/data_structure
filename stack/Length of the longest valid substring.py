9700d62e858211bf6d87306bc23f00e437810244
Lovepreet Singh <lovepreet013hundal@gmail.com>
2020-01-10 22:54:17 +0530

    Length of the longest valid substring

---
+++ stack/Length of the longest valid substring.py
@@ -0,0 +1,29 @@
+def findMaxLen(string): 
+	n = len(string) 
+
+	stk = [] 
+	stk.append(-1) 
+
+	result = 0
+
+	for i in xrange(n): 
+	
+		if string[i] == '(': 
+			stk.append(i) 
+
+		else: 
+			stk.pop() 
+	
+			if len(stk) != 0: 
+				result = max(result, i - stk[len(stk)-1]) 
+
+			else: 
+				stk.append(i) 
+
+	return result 
+
+string = "((()()"
+print findMaxLen(string) 
+
+string = "()(()))))"
+print findMaxLen(string) 
