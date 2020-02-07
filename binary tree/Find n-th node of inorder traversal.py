7b521457681c90a454d7f577b72523807101c8ec
Lovepreet Singh <lovepreet013hundal@gmail.com>
2020-02-07 22:36:15 +0530

    123

---
+++ binary tree/Find n-th node of inorder traversal.py
@@ -0,0 +1,38 @@
+class newNode: 
+
+	def __init__(self, data): 
+		self.data = data 
+		self.left = None
+		self.right = None
+		self.visited = False
+
+count = [0] 
+
+def NthInorder(node, n): 
+
+	if (node == None): 
+		return
+
+	if (count[0] <= n): 
+
+		""" first recur on left child """
+		NthInorder(node.left, n) 
+		count[0] += 1
+
+		if (count[0] == n): 
+			print(node.data, end = " ") 
+
+		NthInorder(node.right, n) 
+						
+if __name__ == '__main__': 
+
+	root = newNode(10) 
+	root.left = newNode(20) 
+	root.right = newNode(30) 
+	root.left.left = newNode(40) 
+	root.left.right = newNode(50) 
+
+	n = 4
+
+	NthInorder(root, n) 
+
